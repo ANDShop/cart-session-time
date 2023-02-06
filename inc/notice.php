38709a87b4e9b7a2f1d13f3850957b284d9c26de
shun <shun.soccer.shun@gmail.com>
2023-02-06 23:38:56 +0900

    first commit

---
+++ inc/notice.php
@@ -0,0 +1,8 @@
+<div class='notice notice-success is-dismissible'>
+  <p>
+    <?php echo htmlspecialchars($cst_change_txt, ENT_QUOTES, 'UTF-8'); ?>：
+    <?php echo htmlspecialchars($old_val_d, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($cst_day_txt, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($old_val_h, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($cst_hour_txt, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($old_val_m, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($cst_minute_txt, ENT_QUOTES, 'UTF-8'); ?>
+    →
+    <?php echo htmlspecialchars($time_val_d, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($cst_day_txt, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($time_val_h, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($cst_hour_txt, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($time_val_m, ENT_QUOTES, 'UTF-8'); ?><?php echo htmlspecialchars($cst_minute_txt, ENT_QUOTES, 'UTF-8'); ?>
+  </p>
+</div>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Cart Session Time ===
+Contributors: ANDShop
+Donate link: https://wp.and-bro.com
+Tags: cart session, session, session sessting, woocommerce
+Requires at least: 5.0.0
+Tested up to: 6.1.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Cart Session Time plugin will give you a step to confirm information to proceed to checkout
+
+== Description ==
+
+Cart Session Time is a plugin that allows you to change the retention time of your WooCommerce cart information.
+
+== Installation ==
+
+1. Cart Session Time can be installed directly through your WordPress
+Plugins dashboard.
+1. Click "Add New" and Search for "Cart Checkout Confirm"
+1. Install and Activate
+
+== Frequently asked questions ==
+
+
+
+== Screenshots ==
+
+1. Page settings
+
+== Changelog ==
